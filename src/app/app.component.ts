9f81cdac9a7757eae615d66701564d58602cf9e6
cmartoneNTT <clemente.martone@nttdata.com>
2022-12-23 12:28:57 +0100

    create bper app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'bper-oracle';
+  title = "bper-oracle";
+  name = "Set BPER Oracle iframe";
+  url: string = "https://html.it";
+  urlSafe: SafeResourceUrl;
+
+  constructor(public sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
 }
